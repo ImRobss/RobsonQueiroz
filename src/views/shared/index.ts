7d082ef4ca6af165c6ddcd6abfa513c4537b11fc
ImRobss <robson.queirozwe@gmail.com>
2026-06-22 17:21:09 -0300

    feat(views/shared): add shared components barrel export

---
+++ src/views/shared/index.ts
@@ -0,0 +1,7 @@
+export * from "./Container";
+export * from "./Section";
+export * from "./GlassCard";
+export * from "./Reveal";
+export * from "./SectionHeading";
+export * from "./Icons";
+export * from "./Atmosphere";
